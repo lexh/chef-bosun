35e14396ac702a2bc2d9d8636baf16ca55199316
Lex Herbert <lex.herbert@gmail.com>
2015-07-01 17:32:07 -0700

    adding basic template

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,12 +4,13 @@ default['bosun']['init_style']      = 'runit'
 default['bosun']['tsdb_host']       = '127.0.0.1'
 default['bosun']['tsdb_port']       = '4242'
 
-default['bosun']['conf_dir']        = '/etc/bosun/'
-default['bosun']['log_dir']         = '/var/log/bosun/'
+default['bosun']['conf_dir']        = '/etc/bosun'
+default['bosun']['log_dir']         = '/var/log/bosun'
 default['bosun']['open_files']      = 65535
 
 default['bosun']['config_cookbook'] = 'bosun'
-
+default['bosun']['hostname'] = node.name
+default['bosun']['http_listen'] = ":8070"
 
 # Use golang recipe to install bosun
 default['go']['packages']           = ['bosun.org/cmd/bosun']
